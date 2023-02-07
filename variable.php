7eda3fdcf6ed77488a5256cbb8c9e786efb79c1c
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-07 09:34:57 +0200

    Exercise 3

---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php include"l.header.php" ?>
+<h1>Exercise 3</h1>
+<form>
+    First name <input type="text" name="fname">
+    Date <input type="text" name="Birth Date">
+   
+  Password <input type="password" name="pwd">
+ Submit <input type="submit" name="Submit">
+ Your favorite color <input type="color" id="favcolor" name="favcolor" value="pink">
+  <input type="submit" value="Submit">
+<label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="">
+
+</form>
+
+
+
+<?php include"l.footer.php" ?>
