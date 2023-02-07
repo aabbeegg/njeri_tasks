a1173fd83b2dfaf66a7dbcbf57d571d4c4cc6d48
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-07 09:58:07 +0200

    completed step 1 to 4

--- variable.php
+++ variable.php
@@ -1,20 +1,34 @@
 <?php include"l.header.php" ?>
 <h1>Exercise 3</h1>
+<h3>Hello. You are welcome to my site</h3>
 <form>
+
     First name <input type="text" name="fname">
+    Last name <input type="text" name="fname">
     Date <input type="text" name="Birth Date">
-   
-  Password <input type="password" name="pwd">
- Submit <input type="submit" name="Submit">
- Your favorite color <input type="color" id="favcolor" name="favcolor" value="pink">
-  <input type="submit" value="Submit">
-<label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value=""><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="">
+    Password <input type="password" name="pwd">
+    Your favorite color <input type="color" id="favcolor" name="favcolor" value="pink">
+    Submit <input type="submit" name="Submit">
+    <input type="submit" value="Submit">
 
 </form>
 
+
+
+<?php 
+ $language1="PhP";
+ $language2="HTML";
+function text1(){
+    $language1="PhP";
+    $language2="HTML";
+    echo text1();
+}
+text1();
+echo $language1 . " " . $language2;
+
+
+
+?>
 
 
 <?php include"l.footer.php" ?>
