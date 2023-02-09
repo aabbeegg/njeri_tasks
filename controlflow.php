b136d53b5d94b2aa7b30e0f0af43aba82f185553
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-09 13:24:11 +0200

    completed task 4

--- controlflow.php
+++ controlflow.php
@@ -1,24 +1,25 @@
-
+<?php include"l.header.php" ?>
 <?php
 // Prints the month 
- echo date('F') . "<br>";
+echo date('F') . "<br>";
 //echo "Not August, this is . " + gmdate(" F") + " . so I don't have any holidays";
-if ( date('F')) {
-   echo "It's August, so it's still holiday.";
-  } else {
-  echo  "Not August, this is . " + gmdate(" F") + " . so I don't have any holidays";
-  }
+if (date('F')) {
+  echo "Not August, this is February so I don't have any holidays";
+} else {
+  echo "It's August, so it's still holiday.";
+}
 ?>
 <br>
 <br>
-  <?php
-function output1() {
-$color = "red";
-if ($color="red") {
-   echo "The color is red.";
+<?php
+function output1()
+{
+  $color = "red";
+  if ($color = "red") {
+    echo "The color is red.";
   } else {
     echo "The color is not red.";
-  } 
+  }
   echo output1();
 }
 
@@ -34,42 +35,48 @@ $excellent = 80;
 $great = 70;
 $good = 60;
 $fail = 50;
-if ($score >80) {
-    echo "Excellent!";
-   } else if($score>=70) {
-     echo "Great!";
-   } else if ($score >= 60){
-    echo "Good!";
-   } else if($score<=50){
-    echo "Fail";
-   }
-   
-?>
-<form>
-   <form method="get" action="">
-   
-Name: <input type="text" name="name" required>
-Age: <input type="text" name="age" required><br>
-<input type="submit" name="submit" value="Submit"> 
-<?php
-$age;
-if ($age>18) {
-    echo "You're eligible to vote";
-   } else {
-     echo "You're not eligible to vote";
-   }
-?>
-</form>
-<?php
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
-$browser = "Mozilla";
-switch($user_agent){
-   case (strpos($user_agent, 'Mozilla') == true);
+if ($score > 80) {
+  echo "Excellent!";
+} else if ($score >= 70) {
+  echo "Great!";
+} else if ($score >= 60) {
+  echo "Good!";
+} else if ($score <= 50) {
+  echo "Fail";
 }
 
 ?>
+
+<form method="post" action="">
+
+  Name: <input type="text" name="name" required>
+  Age: <input type="text" name="age" required><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+<?php
+
+if (isset($_POST['submit'])) {
+
+  $age = $_POST["age"];
+  if ($age > 18) {
+    echo "You're eligible to vote";
+  } else {
+    echo "You're not eligible to vote";
+  }
+}
+?>
+
+<?php
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+$browser = "Mozilla";
+switch ($user_agent) {
+  case (strpos($user_agent, 'Mozilla') == true);
+}
+
+
+
 /*Write a program to get inputs (age and name) from the user and based on their age, 
 decide if he/she is eligible for voting. 
 (18 or more than 18 years is eligible for voting,)*/
-
-
+?>
+<?php include"l.footer.php" ?>
