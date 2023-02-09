d0ae345756903fb795d76c722725becdcab1b9d3
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-10 00:15:43 +0200

    incorporated header and footer

--- array.php
+++ array.php
@@ -1,3 +1,4 @@
+<?php include"l.header.php" ?>
 <?php
 $courses=array("PHP","HTML","JavaScript","CMS","Project");
 echo "I like " . $courses[0] . ", " . $courses[1] . " , " . $courses[2] . " , " . $courses[3] . " , " . $courses[4] .".";
@@ -97,3 +98,4 @@ echo $temp_array[$i].", ";
 }
 
 ?>
+<?php include"l.footer.php" ?>
