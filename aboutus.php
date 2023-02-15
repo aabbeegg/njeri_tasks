1dc5f3fef71bb92e5662bf9a387d96ac674a1ff4
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-15 14:43:14 +0200

    restored deleted files etc

---
+++ aboutus.php
@@ -0,0 +1,12 @@
+<?php include"l.header.php"
+?>
+
+
+
+<h2>About Us</h2>
+<p>
+It lists websites along with information about their content.<br>
+CSS is quite easy to use.<br>
+AboutUs.com has since become a wiki for more than just websites.<br>
+
+<?php include"l.footer.php" ?>
