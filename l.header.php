df2ed1d308d0c7a26ca8399a651d0bbc10dbbdbb
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-03-06 11:11:49 +0200

    edited

--- l.header.php
+++ l.header.php
@@ -1,47 +1,26 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> 
- <script src = "1st-JS.js">
- </script>
-<div class="container" >
-    <div class="row">
-  <nav class="navbar navbar-expand-lg" style="background-color: pink" >
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="exe1.php">ex1.php</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="exe1.php">ex1.php</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../crude1/create.php">create.php</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="variable.php">Variable php</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="controlflow.php">Control Flow</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="array.php">Array php</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../njeri_tasks/create.php">Create php</a>
-        </li>
-        
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+
+<div class="col-sm-9">
+      <h2>What is bootstrap ? </h2> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/440px-Bootstrap_logo.svg.png" align="right" height="150" width="150">
+      <p>Bootstrap is a free and open-source CSS framework directed at responsive,
+         mobile-first front-end web development. It contains CSS- and (optionally) 
+         JavaScript-based design templates for typography, forms, buttons, navigation
+          and other interface components.
+
+        <p>Bootstrap is the sixth-most-starred project on GitHub, with more than 135,000 stars, behind freeCodeCamp (almost 307,000 stars) and marginally behind Vue.js framework.[2] According to Alexa Rank, Bootstrap getbootstrap.com
+         is in the top-2000 in US while vuejs.org is in top-7000 in US.[3]</p></p>
+      <br>
+      <h2>Bootstrap 4</h2>
+      <p>
+
+        Bootstrap 4
+        Mark Otto announced Bootstrap 4 on October 29, 2014.
+        The first alpha version of Bootstrap 4 was released on August 19, 2015.[10]
+         The first beta version was released on 10 August 2017.[11] 
+         Mark suspended work on Bootstrap 3 on September 6, 2016, to free up 
+         time to work on Bootstrap 4.
+         Bootstrap 4 was finalized on January 18, 2018.
+      </p>
+      
+      <p>Article Src: https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)</p>
+
     </div>
-  </div>
-</nav>
-</div> 
