0d32b1c8705eacd56c84da521a5fa77df20e55f5
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-03-10 23:52:04 +0200

    edited

--- header.php
+++ header.php
@@ -60,8 +60,11 @@ body{
                         <a class="nav-link" href="variables.php">Variable</a>
                     </li>
                     
-        
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com/aabbeegg/njeri_tasks.git">My git repos</a>
+                    </li>
       </ul>
+      
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
